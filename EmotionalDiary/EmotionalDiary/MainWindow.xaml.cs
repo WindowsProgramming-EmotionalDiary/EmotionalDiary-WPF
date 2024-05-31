d1641916846e87b21ed1d03cb7bf39631762c234
YuRimBang <qkddbfla1@naver.com>
2024-05-31 12:28:50 +0900

    init

---
+++ EmotionalDiary/EmotionalDiary/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EmotionalDiary
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            // 초기 페이지 로드
+            MainFrame.Navigate(new Uri("MainPage.xaml", UriKind.Relative));
+        }
+
+        private void TabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (e.Source is TabControl)
+            {
+                TabItem selectedTab = ((TabControl)e.Source).SelectedItem as TabItem;
+                if (selectedTab != null)
+                {
+                    string pageUri = selectedTab.Tag.ToString();
+                    MainFrame.Navigate(new Uri(pageUri, UriKind.Relative));
+                }
+            }
+        }
+    }
+}
